4524d5158068840e576c97dd54d35fbdc63891a3
mconklin-boop <mconklin@conklininvestments.com>
2026-03-20 15:48:23 -0600

    Create globals.css

---
+++ styles/globals.css
@@ -0,0 +1,20 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  background: #050505;
+  color: #f4ead2;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+}
