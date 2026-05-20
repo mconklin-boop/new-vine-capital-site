625e029dce49d7507976ae256881ba7a5843a127
mconklin-boop <mconklin@conklininvestments.com>
2026-05-20 12:44:09 -0600

    Clear Supabase portal cookies on logout

--- pages/api/portal/logout.js
+++ pages/api/portal/logout.js
@@ -1,9 +1,10 @@
-import { clearSessionCookie, getPortalSession, logPortalEvent } from "../../../lib/portalAuth";
+import { clearSessionCookies, getPortalSession, logPortalEvent } from "../../../lib/portalAuth";
 
-export default function handler(req, res) {
-  const user = getPortalSession(req);
-  if (user) logPortalEvent({ type: "logout", userId: user.id, email: user.email });
-  res.setHeader("Set-Cookie", clearSessionCookie());
+export default async function handler(req, res) {
+  const user = await getPortalSession(req);
+  if (user) await logPortalEvent({ type: "logout", userId: user.id, email: user.email });
+
+  res.setHeader("Set-Cookie", clearSessionCookies());
   res.writeHead(302, { Location: "/investor-portal" });
   res.end();
 }
